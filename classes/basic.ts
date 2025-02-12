c9b61628013a01f69ab084d12cec6a35982f292f
Enzo Bustamante <enzobustamante895@gmail.com>
2025-02-12 19:04:09 -0300

    feat: add extends

--- classes/basic.ts
+++ classes/basic.ts
@@ -1,17 +1,32 @@
 (() => {
   class Avenger {
+    // es mas rapido y facil definir las propiedades de la clase en el constructor
     // private name: string;
     // public team: string;
     // public realName?: string;
     static avgAge: number = 35;
+    static getAvgAge() {
+      return `The average age of the avengers is: ${this.name} `;
+    }
 
     constructor(
       private name: string,
       public team: string,
       public realName?: string
     ) {}
+
+    // hacer metodos
+
+    public bio() {
+      return `El Superheroe: "${this.name}" pertenece al equipo de ${this.team}`;
+    }
   }
-  const antman: Avenger = new Avenger("Antman", "Cap", "Scott Lang");
+  const antman: Avenger = new Avenger(
+    "Antman",
+    "Capitan America",
+    "Scott Lang"
+  );
   console.log(antman);
-  console.log(Avenger.avgAge);
+  console.log(Avenger.getAvgAge);
+  console.log(antman.bio());
 })();
--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 "use strict";
+(() => { })();
 (() => {
     class Avenger {
         constructor(name, team, realName) {
@@ -6,10 +7,52 @@
             this.team = team;
             this.realName = realName;
         }
+        static getAvgAge() {
+            return `The average age of the avengers is: ${this.name} `;
+        }
+        bio() {
+            return `El Superheroe: "${this.name}" pertenece al equipo de ${this.team}`;
+        }
     }
     Avenger.avgAge = 35;
-    const antman = new Avenger("Antman", "Cap", "Scott Lang");
+    const antman = new Avenger("Antman", "Capitan America", "Scott Lang");
     console.log(antman);
-    console.log(Avenger.avgAge);
+    console.log(Avenger.getAvgAge);
+    console.log(antman.bio());
 })();
+(() => {
+    class Avenger {
+        constructor(name, realName) {
+            this.name = name;
+            this.realName = realName;
+            console.log("Constructor Avenger llamado!");
+        }
+        getFullname() {
+            return `${this.name} ${this.realName}`;
+        }
+    }
+    class Xmen extends Avenger {
+        constructor(name, realName, isMutant) {
+            super(name, realName);
+            this.isMutant = isMutant;
+            console.log("Constructor Xmen llamado");
+        }
+        get fullName() {
+            return `${this.name} - ${this.realName}`;
+        }
+        set fullName(name) {
+            if (name.length < 3) {
+                throw new Error("EL nombre debe de ser mayor de 3 letras");
+            }
+            this.name = name;
+        }
+        getFullnameDesdeXmen() {
+            console.log(super.getFullname());
+        }
+    }
+    const wolverine = new Xmen("Wolverine", "Logan", true);
+    console.log(wolverine.fullName);
+    wolverine.getFullnameDesdeXmen();
+})();
+(() => { })();
 //# sourceMappingURL=main.js.map
